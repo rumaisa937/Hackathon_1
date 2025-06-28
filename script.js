94b06136efa8d4cb24a66d20eded3716b1d291c4
divishachn <chndivisha@gmail.com>
2025-06-28 19:18:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,14 @@
 var map = L.map('map').setView([43.65, -79.38], 8);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-navigator.geolocation.watchPosition(success, error);
+var marker = L.marker([43.77, -79.23]).addTo(map);
 
-function success(position)
-{
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const accuracy = position.coords.accuracy;
-
-    let marker = L.marker([lat, lon]).addTo(map);
-    let circle = L.circle([lat, lon]).addTo(map);
-
-    map.fitBounds(circle.getBounds());
-    }
-
-farms = [];
-
-async function getFarms()
-{
-    const response = await fetch('http://localhost:8080/api/farms'); 
-    const farms = await response.json();
-    //const map = L.map('map').setView([43.65, -79.38], 8);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
-    const farmSelect = document.getElementById('farm-select');
-
-    farms.forEach(farm => {
-    L.marker([farm.lat, farm.lng])
-      .addTo(map)
-      .bindPopup(`<b>${farm.name}</b><br>${farm.produce}`);
-
-    const option = document.createElement('option');
-    option.value = farm.id;
-    option.textContent = farm.name;
-    farmSelect.appendChild(option);
-  });
-
-}
-getFarms();
-
+fetch('farms.csv').then(response => response.text()).then(csvText => {const lines = csvText.split('\n');
+    lines.forEach((line, index) => {const values = line.split(',');
+        console.log(values[2]);
+        console.log(values[3]);});
+        L.marker([values[2], values[3]]).addTo(map).bindPopup(`<strong>${name}</strong>`);
+    
+    }).catch(error => {console.error('Error fetching farms data:', error);});
